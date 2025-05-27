76d1ba46f3f19395a94c31fccba3fb364c3d5c9f
hseoy05 <tjdus121234@korea.ac.kr>
2025-05-27 12:50:11 +0900

    jj

--- frontend/vite-project/src/pages/Create/WriteContent.jsx
+++ frontend/vite-project/src/pages/Create/WriteContent.jsx
@@ -2,18 +2,35 @@ import {React, useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 const WriteContent = () => {
+    const navigate = useNavigate();
+
+    const [content, setContent] = useState({
+        title: '',
+        body: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setContent({ ...content, [name]: value});
+    };
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        navigate('/home');
+    }
 
     return (
         <div>
             <h2>게시글 쓰기</h2>
-            <br></br>
-            <p>Title</p>
-            <input type="text" placeholder="title" />
-            <p>Content</p>
-            <textarea placeholder="content" rows="15" cols="50"></textarea>
-            <div>
-                <p><button>저장</button></p>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <p>Title</p>
+                    <input name = "title" type="text" placeholder="title" onChange={handleChange}/>
+                    <p>Content</p>
+                    <textarea name = "content" placeholder="content" rows="15" cols="50" onChange={handleChange}></textarea>
+                </div>
+                <p><button type="submit" onClick={()=>{setContent}}>저장</button></p>
+            </form>
         </div>
     ); 
 };
