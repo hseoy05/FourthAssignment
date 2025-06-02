34b59a36beb8e028da067d3d9b575094849317c6
hseoy05 <tjdus121234@korea.ac.kr>
2025-06-02 23:28:49 +0900

    delete Content

---
+++ frontend/vite-project/src/pages/Delete/DeleteContent.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {userParams, useNavigate} from 'react-router-dom';
+
+const Delete =()=>{
+    cosnt [nowUserId, setNowUserId] = useState('');
+    const {id}= useParam();
+    const navigate = useNavigate();
+}
+
+export default Delete;
